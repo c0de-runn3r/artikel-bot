6e95c111b41753e7d00e730e14a0c95268c76f08
c0de-runn3r <109829568+c0de-runn3r@users.noreply.github.com>
2022-09-04 21:57:59 +0200

    first commit

---
+++ artikel-bot.py
@@ -0,0 +1,50 @@
+from email import message
+from unittest.result import failfast
+import requests
+from bs4 import BeautifulSoup
+import re
+
+inp_word = input('Print your word here:')
+word = inp_word.capitalize()
+
+def check(word):
+    check_der = requests.get('https://der-artikel.de/der/' + word + '.html')
+    if check_der:
+        return "der"
+    else:
+        check_das = requests.get('https://der-artikel.de/das/' + word + '.html')
+        if check_das:
+            return "das"
+        else:
+            check_die = requests.get('https://der-artikel.de/die/' + word + '.html')
+            if check_die:
+                return "die"
+            else:
+                return None
+if check(word) == None:
+    print("Sorry, I don't know this word :(")
+else:
+    print(check(word) + " " + word)
+
+def check_art():
+    if check(word) == "der":
+        return 'https://der-artikel.de/der/' + word + '.html'
+    elif check(word) == "das":
+        return 'https://der-artikel.de/das/' + word + '.html'
+    elif check(word) == "die":
+        return 'https://der-artikel.de/die/' + word + '.html'
+    else:
+        return None
+
+url = check_art()
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+element = soup.find("h3", class_="mb-5")
+form_el = element.text.strip()
+
+el = re.split('\W+', form_el)
+trnsl = el[-1]
+print("eng: " + trnsl)
+
+
+
